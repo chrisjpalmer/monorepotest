974b8e4429705e7ef02351a104f11dad387f664b
Christopher Palmer <chrisjpalmer@optusnet.com.au>
2019-02-09 20:20:05 +1100

    added the google logger as a dependency

--- pkg1/main.go
+++ pkg1/main.go
@@ -1,7 +1,24 @@
 package pkg1
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/google/logger"
+)
+
+const logPath = "/test.log"
 
 func Pkg1Function() {
 	fmt.Println("Hello Pkg1!")
+	fmt.Println("Going to log something")
+	lf, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	if err != nil {
+		logger.Fatalf("Failed to open log file: %v", err)
+	}
+	defer lf.Close()
+
+	defer logger.Init("LoggerExample", true, true, lf).Close()
+
+	logger.Info("I'm about to do something!")
 }
